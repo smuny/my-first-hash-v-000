03db3d6352b8d48a19970b31b0c3ddda99435590
Sharon Mun <sharonymun@gmail.com>
2018-06-21 22:31:08 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -26,7 +26,7 @@ def adding
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
-  }
+                    }
 shipping_manifest["muskets"] = 2
 shipping_manifest["gun powder"] = 4
 
