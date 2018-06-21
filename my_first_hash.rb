d3db5a418f7b1395a1340761767d6cbfedacef78
Sharon Mun <sharonymun@gmail.com>
2018-06-21 22:27:10 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -27,10 +27,10 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  shipping_manifest = {"muskets" => 2}
-  shipping_manifest = {"gun powder" => 4}
-
-
+  shipping_manifest = {
+                  "muskets" => 2
+                  "gun powder" => 4
+                 }
  shipping_manifest
 
 end
