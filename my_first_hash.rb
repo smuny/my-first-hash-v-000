086b4e4fff0c2cff6ce7e42ee0a2b820da54ea45
Sharon Mun <sharonymun@gmail.com>
2018-06-21 22:29:39 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -26,9 +26,9 @@ def adding
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
-                 
+  }
 shipping_manifest["muskets"] = 2
 shipping_manifest["gun powder"] => 4
-                 }
+
 shipping_manifest
 end
